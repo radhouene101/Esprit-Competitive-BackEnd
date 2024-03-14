c40ad2a5b1b3fb5d1c9d6d025d168ccba1394490
ahmedbelhajhassen22@gmail.com <ahmedbelhajhassen22@gmail.com>
2024-03-14 23:39:20 +0100

    project setup

---
+++ Pi-Cloud/src/test/java/com/apex/picloud/PiCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.picloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
