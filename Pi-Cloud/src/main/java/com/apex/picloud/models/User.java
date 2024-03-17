187ec7fe86634e604b533f6524353a7bc1cdf104
ahmedbelhajhassen22@gmail.com <ahmedbelhajhassen22@gmail.com>
2024-03-17 21:22:31 +0100

    creation de l entite user

---
+++ Pi-Cloud/src/main/java/com/apex/picloud/models/User.java
@@ -0,0 +1,14 @@
+package com.apex.picloud.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     private String name;
+     private String email;
+     private String phone;
+}
