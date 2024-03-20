1b8fe5e792b7fabe8239ba8145dad666819ef83d
ahmedbelhajhassen22@gmail.com <ahmedbelhajhassen22@gmail.com>
2024-03-20 02:42:12 +0100

    token yekhdem

--- Pi-Cloud/src/main/java/com/apex/picloud/configurations/WebSecurityConfiguration.java
+++ Pi-Cloud/src/main/java/com/apex/picloud/configurations/WebSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.apex.picloud.configurations;
 
+import com.apex.picloud.filters.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,11 +13,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfiguration {
+
+@Autowired
+private JwtRequestFilter requestFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -31,7 +38,10 @@ public class WebSecurityConfiguration {
                 .authenticated().and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+                .and()
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)//houni staamlna token
+
+                .build();
     }
 
     @Bean
---
+++ Pi-Cloud/src/main/java/com/apex/picloud/filters/JwtRequestFilter.java
@@ -0,0 +1,53 @@
+package com.apex.picloud.filters;
+
+import com.apex.picloud.services.jwt.UserDetailsServiceImpl;
+import com.apex.picloud.utils.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeadr = request.getHeader("Authorization");
+        String token = null;
+        String username= null;
+        if(authHeadr!=null && authHeadr.startsWith("Bearer")){
+            token= authHeadr.substring(7);
+            username=jwtUtil.extractUsername(token);//mil token njib eli nheb
+        }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+
+    }
+}
