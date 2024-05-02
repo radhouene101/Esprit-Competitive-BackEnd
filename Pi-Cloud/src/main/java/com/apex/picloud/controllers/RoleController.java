481810731d59a71c442251eeb16e71270ada4ce1
ahmedbelhajhassen22@gmail.com <ahmedbelhajhassen22@gmail.com>
2024-05-02 01:43:35 +0100

    moudir commit

--- Pi-Cloud/src/main/java/com/apex/picloud/controllers/RoleController.java
+++ Pi-Cloud/src/main/java/com/apex/picloud/controllers/RoleController.java
@@ -1,13 +1,15 @@
 package com.apex.picloud.controllers;
 
 import com.apex.picloud.dtos.RoleDTO;
+import com.apex.picloud.models.User;
 import com.apex.picloud.services.IRoleService;
 import com.apex.picloud.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RoleController {
@@ -19,4 +21,21 @@ public class RoleController {
         RoleDTO createdRole = roleService.createRole(roleDTO);
         return ResponseEntity.ok(createdRole);
     }
+    @DeleteMapping("/deleteRole/{idRole}")
+    public void DeleteRole(@PathVariable Long idRole) {
+         roleService.DeleteRole(idRole);
+ }
+    @PostMapping("/updateRole/{id}")
+    public RoleDTO updateRole(@PathVariable Long id,RoleDTO role){
+            return roleService.updateRole(id,role);
+    }
+    @GetMapping("list-all-roles")
+    public List<RoleDTO> showAllRoles(){
+        return roleService.showAll();
+    }
+    @PostMapping("/affect-role-to-user/{userId}/{roleId}")
+    public void affectRoleToUser(@PathVariable Long userId , @PathVariable Long roleId){
+        roleService.affectRoleToUser(roleId,userId);
+    }
+
 }
--- Pi-Cloud/src/main/java/com/apex/picloud/services/RoleService.java
+++ Pi-Cloud/src/main/java/com/apex/picloud/services/RoleService.java
@@ -2,10 +2,16 @@ package com.apex.picloud.services;
 
 import com.apex.picloud.dtos.RoleDTO;
 import com.apex.picloud.models.Role;
+import com.apex.picloud.models.User;
 import com.apex.picloud.repositories.RoleRepository;
+import com.apex.picloud.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,6 +19,8 @@ public class RoleService {
 
     @Autowired
     RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
     public RoleDTO createRole(RoleDTO roleDTO) {
         Role role = new Role();
         role.setName(roleDTO.getName());
@@ -20,5 +28,40 @@ public class RoleService {
         RoleDTO savedRoleDTO = new RoleDTO();
         savedRoleDTO.setName(savedRole.getName());
         return savedRoleDTO;
+    }
+
+    public void  DeleteRole(Long id){
+        roleRepository.deleteById(id);
+
+    }
+    public RoleDTO updateRole(Long id,RoleDTO roleDTO){
+        Role role = roleRepository.findById(id).get();
+        role.setName(roleDTO.getName());
+        roleRepository.save(role);
+        return roleDTO;
+
+    }
+
+    public List<RoleDTO> showAll() {
+        List<Role> roleList =roleRepository.findAll();
+        List<RoleDTO> roleDtoList =new ArrayList<>();
+        RoleDTO roleDto =new RoleDTO();
+        roleList.forEach(role -> {
+                roleDto.setId(role.getId());
+                roleDto.setName(role.getName());
+                roleDtoList.add(roleDto);
+
+                }
+        );
+        return roleDtoList;
+    }
+    public void affectRoleToUser(Long roleId , Long userid){
+        User user = userRepository
+                .findById(userid)
+                .orElseThrow(()-> new EntityNotFoundException("user provided doesnt exist :) c radhouene"));
+        Role role = roleRepository.findById(roleId)
+                .orElseThrow(()-> new EntityNotFoundException("this role doesnt exist!"));
+        user.getRoles().add(role);
+        userRepository.save(user);
     }
 }
