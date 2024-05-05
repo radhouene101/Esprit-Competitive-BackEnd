662247b3cb445b278b47074d7b346c9ec2dcc710
radhouene101 <radhouene.berrzig@esprit.tn>
2024-05-05 18:48:30 +0100

    before6

--- Pi-Cloud/src/main/java/com/apex/picloud/models/Role.java
+++ Pi-Cloud/src/main/java/com/apex/picloud/models/Role.java
@@ -2,12 +2,16 @@ package com.apex.picloud.models;
 
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,11 +20,6 @@ public class Role {
     //@JsonIgnoreProperties("user_roles")
     private Set<User> user_roles = new HashSet<>();
     private String name;
-    public Role() {}
 
-    public Role(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 // Constructors, getters, and setters
 }
